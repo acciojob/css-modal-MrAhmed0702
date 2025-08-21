7195b8f96f3feae4de85cd1192e320d0c6eba01d
Ahmed Mohammed Jayanti Mochi <amochi670@rku.ac.in>
2025-08-21 21:57:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -11,4 +11,9 @@ document.addEventListener('DOMContentLoaded', () => {
     modal.style.display = 'none';
   });
 
+  window.addEventListener('click', (event) => {
+    if (event.target === modal) {
+      modal.style.display = 'none';
+    }
+  });
 });
