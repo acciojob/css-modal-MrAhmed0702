027f683f2b5f07e9eabcd8af862b2494cfa69cf1
Ahmed Mohammed Jayanti Mochi <amochi670@rku.ac.in>
2025-08-21 21:43:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-//your JS code here. If required.
+document.addEventListener('DOMContentLoaded', () => {
+  const openBtn = document.getElementById('openModal');
+  const modal = document.getElementById('myModal');
+  const closeBtn = document.querySelector('.close-modal');
+
+  openBtn.addEventListener('click', () => {
+    modal.style.display = 'block';
+  });
+
+  closeBtn.addEventListener('click', () => {
+    modal.style.display = 'none';
+  });
+
+});
